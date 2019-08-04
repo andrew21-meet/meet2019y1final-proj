662091ef34df26fa94349af689692b04c2a58efe
andrew21-meet <andrew21@meet.mit.edu>
2019-08-04 11:29:01 +0300

    Stopping here

---
+++ lab_final-proj.py
@@ -0,0 +1,40 @@
+#Hangman Trivia!!
+
+#make variables for
+#[questions]
+#right_answer
+#wrong_answers
+#bad_pictures
+#good_pictures
+#name
+#
+#
+
+
+#register all graphics and pictures
+
+
+#ask user to input name
+
+#welcome the user to hang man
+
+#do the following 5 times
+
+    #ask user question
+
+    #show answers
+
+    #take answers
+        #if right
+            #add  1 good_picture to the drawing
+        #else
+            #add 1  bad_picture to the drawing
+
+#if loses more the wins
+    #print you lose
+#else wins more then loses
+    #print you win
+
+#congratulate if user wins
+#dont congratulate if user loses
+
