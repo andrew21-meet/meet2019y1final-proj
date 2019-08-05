776ac801802f86f5d0e9e960596d467aa370bac3
andrew21-meet <andrew21@meet.mit.edu>
2019-08-05 10:30:34 +0300

    Stopping here

--- lab_final-proj.py
+++ lab_final-proj.py
@@ -37,4 +37,153 @@
 
 #congratulate if user wins
 #dont congratulate if user loses
+import turtle
+turtle.bgcolor("green")
+
+turtle.register_shape("car.gif")
+turtle.register_shape("cow.gif")
+turtle.register_shape("electric car.gif")
+turtle.register_shape("factory.gif")
+turtle.register_shape("cow-farting.gif")
+turtle.register_shape("fire.gif")
+turtle.register_shape("flowers.gif")
+turtle.register_shape("plastic trash.gif")
+turtle.register_shape("recycling factory.gif")
+turtle.register_shape("sea.gif")
+
+car = turtle.clone ()
+car.penup()
+car.hideturtle()
+car.goto(-600, -100)
+
+elec_car = turtle.clone()
+elec_car.penup()
+elec_car.hideturtle()
+elec_car.goto(-600, -100)
+
+
+cow = turtle.clone()
+cow.penup()
+cow.hideturtle()
+cow.goto(-300, -100)
+
+farting_cow = turtle.clone()
+farting_cow.penup()
+farting_cow.hideturtle()
+farting_cow.goto(-300, -100)
+
+factory = turtle.clone()
+factory.penup()
+factory.hideturtle()
+factory.goto(0, -100)
+
+refactory = turtle.clone()
+refactory.penup()
+refactory.hideturtle()
+refactory.goto(0, -100)
+
+fire = turtle.clone()
+fire.penup()
+fire.hideturtle()
+fire.goto(300, -100)
+
+flowers = turtle.clone()
+flowers.penup()
+flowers.hideturtle()
+flowers.goto(300, -100)
+
+sea = turtle.clone()
+sea.penup()
+sea.hideturtle()
+sea.goto(600, -100)
+
+trash = turtle.clone()
+trash.penup()
+trash.hideturtle()
+trash.goto(600, -100)
+
+
+null = turtle.clone()
+null.penup()
+null.hideturtle()
+null.goto(1000,1000)
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Question:
+     def __init__(self, prompt, answer, turtle_shape, bad_turtle):
+          self.prompt = prompt
+          self.answer = answer
+          self.turtle_shape = turtle_shape
+question_prompts = [
+     "\nwhich country has the most pollution?\n(a) China\n(b) Brazil\n(c) United States\n(d) Indonesia\n",
+     "\nHow many years of oil is left in the world?\n(a) 101 years\n(b) 53 years\n(c) 20 years\n(d) 69 years\n",
+     "\nHow much oil is left in the world?\n(a) 6.003 trillion barrels\n(b) 3.775 trillion barrels\n(c) 0.804 trillion barrels\n(d) 1.688 trillion barrels\n",
+     "\nWho is the biggest oil producer in the world?\n(a) Saudi Arabia\n(b) USA\n(c) Russia\n(d) China\n",
+     "\nHow many years of gas are left in the world?\n(a)200 years\n(b) 86 years\n(c) 115 years\n(d)98 years\n",
+     "\nwhat is the easiest plastic code to recycle?\n(a) 3\n(b) 6\n(c) 7\n(d) 1\n",
+     "\nWhat is cutting of trees called?\n(a) hewing\n(b) cutzing\n(c) CTs\n(d) falling\n",
+     "\nWhat is the largest oil company in Canada?\n(a)Encana Corporation\(b)Suncor Energy\n(c)Husky Energy\n(d)Enbridge\n",
+     "\nWhat is the largest oil company in Canada?\n(a)water\n(b)sand\n(c)dust\n(d)stones\n",
+     "\nWhat is the top cause of air pollution?\n(a)Emission from Vehicles\n(b)Poisonous Gas\n(c)Combustion of Fossil Fuels\n(d)Pollution From AC\n",
+     
+
+     
+
+
+]
+questions = [
+     Question(question_prompts[0], "a", elec_car, car),
+     Question(question_prompts[1], "b", cow, farting_car),
+     Question(question_prompts[2], "d", refactory, factory),
+     Question(question_prompts[3], "a", flowers, fire),
+     Question(question_prompts[4], "c", sea, trash),
+     Question(question_prompts[5], "d", null),
+     Question(question_prompts[6], "a", null),
+     Question(question_prompts[7], "b", null),
+     Question(question_prompts[8], "c", null),
+     Question(question_prompts[9], "b", null),
+     
+]
+
+def run_quiz(questions):
+    print("\n")
+    for question in questions:
+            
+          answer = input(question.prompt)
+          if answer == question.answer:
+            print("the answer is correct!")
+            question.turtle_shape.showturtle()
+          else:
+            print("the answer is incorrect..")
+            print("the right answer is " + question.answer)
+            question.bad_shape.showturtle()
+            
+
+
+run_quiz(questions)
+
+
+
+
+
+    
+
+
+
+
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.register_shape("factory.gif")
+turtle.penup()
+turtle.hideturtle()
+turtle.goto(-100, -100)
+turtle.showturtle()
+turtle.shape("factory.gif")
